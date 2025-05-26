21ea9f6dc041e51e69a7d0ce977fb4ef03e827de
manel <manel@gmail.com>
2025-05-26 19:33:02 +0200

    SpringBoot creado

---
+++ src/main/java/es/progcipfpbatoi/batoiflix_prg/BatoiflixPrgApplication.java
@@ -0,0 +1,13 @@
+package es.progcipfpbatoi.batoiflix_prg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatoiflixPrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatoiflixPrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/progcipfpbatoi/batoiflix_prg/BatoiflixPrgApplicationTests.java
@@ -0,0 +1,13 @@
+package es.progcipfpbatoi.batoiflix_prg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatoiflixPrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
